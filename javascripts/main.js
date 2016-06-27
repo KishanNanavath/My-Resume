393e175561fb8e6bad04c1b6949047ad9d6e8aa0
KishanNanavath <nbalkishan@gmail.com>
2016-06-27 22:36:50 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+/**
+ * Created by Kishan on 08-Jun-16.
+ */
+var moviesapp = angular.module('moviesApp',[]);
+
+moviesapp.service('myServ', ['$http',function ($http) {
+    return{Async: function (link) {
+        return $http.get(link);
+    }};
+}]);
+
+moviesapp.controller('mainCtrl',['$scope','$http','myServ',function ($scope,$http,myServ) {
+    var self = this;
+    self.link = 'https://api.themoviedb.org/3/search/movie?api_key=2c3bc0f5d4658e72d743844770976b5b&query=';
+    self.data = {};
+    self.noPoster = 'https://assets.tmdb.org/assets/7f29bd8b3370c71dd379b0e8b570887c/images/no-poster-w185-v2.png';
+    self.base_url = 'https://image.tmdb.org/t/p/w185';
+    myServ.Async(self.link+'king')
+        .then(function (response) {
+            self.data = response.data.results;
+        }, function (response) {
+            console.log(response);
+        });
+}]);
