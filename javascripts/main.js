f5347fc62c03bbedb28e2905e5848c6e7e03de64
KishanNanavath <nbalkishan@gmail.com>
2016-07-02 16:50:06 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,7 +11,7 @@ moviesapp.service('myServ', ['$http',function ($http) {
 
 moviesapp.controller('mainCtrl',['$scope','$http','myServ',function ($scope,$http,myServ) {
     var self = this;
-    self.link = 'https://api.themoviedb.org/3/search/person?api_key=2c3bc0f5d4658e72d743844770976b5b&query=';
+    self.link = 'https://api.themoviedb.org/3/search/movie?api_key=2c3bc0f5d4658e72d743844770976b5b&query=';
     self.data = {};
     self.noPoster = 'https://assets.tmdb.org/assets/7f29bd8b3370c71dd379b0e8b570887c/images/no-poster-w185-v2.png';
     self.base_url = 'https://image.tmdb.org/t/p/w185';
